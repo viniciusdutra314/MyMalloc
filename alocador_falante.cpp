ea8c3be3af42dcc4adc1b47473bcea09f1281e17
viniciusdutra314 <dutravinisousa2@usp.br>
2025-09-01 14:03:21 +0000

    feat: alocador falante

---
+++ alocador_falante.cpp
@@ -0,0 +1,26 @@
+#include <memory>
+#include <print>
+#include <vector>
+
+template <typename T>
+class AlocadorFalante{
+    std::allocator<T> default_alloc;
+    public:
+        using value_type = T;
+        T* allocate(size_t n){
+            std::println("Im allocating {} elements",n);
+            return default_alloc.allocate(n);
+        }
+        void deallocate(T* ptr,size_t n){
+            std::println("I'm deallocating");
+            default_alloc.deallocate(ptr,n);
+        }
+};
+
+
+int main(){
+    std::vector<int,AlocadorFalante<int>> vec;
+    for (int i=0;i<5;i++){
+        vec.push_back(i);
+    }
+}
